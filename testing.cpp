f9c8c20418b4bff58335ec97e51d2d50c7fc06cf
jsonfrench <jsonfrench@gmail.com>
2024-04-09 12:19:21 -0400

    created a testing file for testing code from the book i'm using to learn c++

---
+++ testing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    enum card_types {DIAMOND, HEART, SPADES, CLUBS}
+
+    card_types card1 = DIAMOND;
+
+    cout << "and we're off...";
+    return 0;
+}
